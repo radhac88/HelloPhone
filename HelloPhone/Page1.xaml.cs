acb7cd2e0ad22abe968caf8f563abc5b89094127
Radha <radhac88@gmail.com>
2013-09-04 11:43:16 +0530

    Page titles changed

--- HelloPhone/Page1.xaml.cs
+++ HelloPhone/Page1.xaml.cs
@@ -17,15 +17,12 @@ namespace HelloPhone
 {
     public partial class Page1 : PhoneApplicationPage
     {
-        private static Random random;
         DateTime startTime;
         public Page1()
         {
             InitializeComponent();
             startTime = DateTime.Now;
             CompositionTarget.Rendering += OnCompositionTargetRendering;
-            random = new Random();
-            this.StartFallingSnowAnimation();
         }
         void OnCompositionTargetRendering(object sender, EventArgs args)
         {
@@ -129,70 +126,5 @@ namespace HelloPhone
             storyboard.Begin();
         }
 
-        private Ellipse GenerateEllipse()
-        {
-            Ellipse element = new Ellipse();
-            element.Fill = new SolidColorBrush(Colors.White);
-            element.Height = 8.0;
-            element.Width = 8.0;
-            this.ContentPanel.Children.Add(element);
-            return element;
-        }
-        private Storyboard CreateStoryboard(UIElement element, double to, double toLeft)
-        {
-            Storyboard result = new Storyboard();
-            DoubleAnimation animation = new DoubleAnimation();
-            animation.To = to;
-            Storyboard.SetTargetProperty(animation, new PropertyPath("(Canvas.Top)"));
-            Storyboard.SetTarget(animation, element);
-
-            DoubleAnimation animationLeft = new DoubleAnimation();
-            animationLeft.To = toLeft;
-            Storyboard.SetTargetProperty(animationLeft, new PropertyPath("(Canvas.Left)"));
-            Storyboard.SetTarget(animationLeft, element);
-
-            result.Children.Add(animation);
-            result.Children.Add(animationLeft);
-
-            return result;
-        }
-        private void StartFallingSnowAnimation()
-        {
-            for (int i = 0; i < 20; i++)
-            {
-                Ellipse localCopy = this.GenerateEllipse();
-                localCopy.SetValue(Canvas.LeftProperty, i * 30 + 1.0);
-
-                double y = Canvas.GetTop(localCopy);
-                double x = Canvas.GetLeft(localCopy);
-
-                double speed = 5 * random.NextDouble();
-                double index = 0;
-                double radius = 30 * speed * random.NextDouble();
-
-                localCopy.Opacity = .3 + random.NextDouble();
-
-                CompositionTarget.Rendering += delegate(object o, EventArgs arg)
-                {
-                    index += Math.PI / (180 * speed);
-
-                    if (y < this.ContentPanel.DesiredSize.Height)
-                    {
-                        y += .3 + speed;
-                    }
-                    else
-                    {
-                        y = -localCopy.Height;
-                    }
-
-                    Canvas.SetTop(localCopy, y);
-                    Canvas.SetLeft(localCopy, x + radius * Math.Cos(index));
-                    Storyboard animation = this.CreateStoryboard(localCopy, y, x + radius * Math.Cos(index));
-                    Storyboard.SetTarget(animation, localCopy);
-                    animation.Begin();
-
-                };
-            }
-        }
     }
 }
